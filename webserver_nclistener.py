2f6ad1fe2bd17f6dd680b9a106e59e2b2f7e8121
buckdray <buckdray@gmail.com>
2023-04-22 07:41:19 -0400

    Webserver code moved to separate function that's threaded, to test comment out spawn_netcatlistener

--- webserver_nclistener.py
+++ webserver_nclistener.py
@@ -10,17 +10,22 @@ NETCAT_PORT = 1337
 webserver = http.server.SimpleHTTPRequestHandler
 
 def spawn_webserver():
-    print('Web-Server started at', WEB_PORT)
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", WEB_PORT), Handler) as httpd:
+        print("Server was started at port", WEB_PORT)
+        httpd.serve_forever()
+
+        
 
 def spawn_netcatlistener():
     print('Netcat Listener started at', NETCAT_PORT)
 
 def main():
     print("Main function")
-    Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", WEB_PORT), Handler) as httpd:
-        print("Server was started at port", WEB_PORT)
-        httpd.serve_forever()
+    t= threading.Thread(target=spawn_webserver)
+    t.start()
+    //spawn_netcatlistener()
+
 
 if __name__ == '__main__':
     main()
