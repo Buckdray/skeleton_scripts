0fc4fb1ecf6589fd0f1deeefda126194c71d73d5
buckdray <buckdray@gmail.com>
2023-04-22 07:34:50 -0400

    Webserver code added

--- webserver_nclistener.py
+++ webserver_nclistener.py
@@ -4,7 +4,7 @@ import requests
 import threading
 import socket
 
-WEB_PORT = 8080
+WEB_PORT = 8000
 NETCAT_PORT = 1337 
 
 webserver = http.server.SimpleHTTPRequestHandler
@@ -15,6 +15,12 @@ def spawn_webserver():
 def spawn_netcatlistener():
     print('Netcat Listener started at', NETCAT_PORT)
 
+def main():
+    print("Main function")
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", WEB_PORT), Handler) as httpd:
+        print("Server was started at port", WEB_PORT)
+        httpd.serve_forever()
 
 if __name__ == '__main__':
     main()
